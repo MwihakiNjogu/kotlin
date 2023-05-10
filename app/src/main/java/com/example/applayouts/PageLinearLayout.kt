55a5bb0adc48764878d05ce9110c56bf311544fe
Mwihaki Njogu <mwihakinjogu79@gmail.com>
2023-05-10 16:26:03 +0300

    layout

---
+++ app/src/main/java/com/example/applayouts/PageLinearLayout.kt
@@ -0,0 +1,11 @@
+package com.example.applayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PageLinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_linear_layout)
+    }
+}
